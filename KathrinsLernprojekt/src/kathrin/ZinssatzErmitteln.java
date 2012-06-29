4aad608f5f0600acfe37de55093ddc0d8587e13e
Kathrin <Kathrin@KSiegmann-NB.fritz.box>
2012-06-29 16:14:55 +0200

    Laufzeit auf 50 Jahre geändert

---
+++ KathrinsLernprojekt/src/kathrin/ZinssatzErmitteln.java
@@ -0,0 +1,37 @@
+package kathrin;
+
+public class ZinssatzErmitteln {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		double dollars = 1000.00;
+		double zinssatz = 0.01;
+		int LAUFZEIT = 50;
+
+		zinssatz = berechneZinssatz(dollars, LAUFZEIT, zinssatz);
+		System.out.println("Nach 50 Jahren bei einem Zinssatz von " + zinssatz
+				* 100 + "Prozent sind Sie Millionär!");
+	}
+
+	public static double berechneZinssatz(double dollars1, int LAUFZEIT1,
+			double zinssatz) {
+
+		while (dollars1 < 1000000) {
+			int i = 0;
+			dollars1 = 1000.00;
+			zinssatz = zinssatz + 0.01;
+			System.out.println(zinssatz + " - " + dollars1);
+
+			while (i < LAUFZEIT1) {
+				dollars1 = dollars1 + dollars1 * zinssatz;
+				i++;
+				System.out.println(dollars1);
+			}
+		}
+		return zinssatz;
+
+	}
+
+}
