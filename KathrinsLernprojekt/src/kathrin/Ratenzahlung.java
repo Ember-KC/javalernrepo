894086013213589682567c9056256c0b9f6dbcd1
Kathrin <Kathrin@KSiegmann-NB.fritz.box>
2012-07-05 21:37:21 +0200

    Modifizierte Version, bei der Anfangssaldo, monatl. Rate und Zinssatz manuell eingegeben werden und der Saldo am Ende auf 0 gestellt wird.

--- KathrinsLernprojekt/src/kathrin/Ratenzahlung.java
+++ KathrinsLernprojekt/src/kathrin/Ratenzahlung.java
@@ -8,25 +8,40 @@ public class Ratenzahlung {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-    double zinssatz = 0.015;
-    int monatsrate = 0;
-    double saldo = 1000.00;
-    int gesamtrate = 0;
-    int monat = 1;
-    
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Bitte geben Sie die monatliche Rate ein!");
-    monatsrate = scan.nextInt();
-   
-    
-    while (saldo > 0) {
-    saldo = saldo + (saldo*zinssatz) - monatsrate;
-    gesamtrate = gesamtrate + monatsrate;
-    System.out.println("Monat " + monat + " Saldo: " + saldo + " Gesamtratenzahlung: " + gesamtrate);    
-    monat = monat + 1;
-    }
-    scan.close();
-    
-	}
+		double zinssatz = 0;
+		double monatsrate = 0;
+		double saldo = 0;
+		double gesamtrate = 0;
+		double endsaldo = 0;
+		int monat = 1;
 
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Bitte geben Sie den Anfangssaldo ein!");
+		saldo = scan.nextDouble();
+
+		System.out.println("Bitte geben Sie die monatliche Rate ein!");
+		monatsrate = scan.nextInt();
+
+		System.out.println("Bitte geben Sie den Zinssatz ein!");
+		zinssatz = scan.nextDouble();
+
+		do {
+			saldo = saldo + (saldo * zinssatz) - monatsrate;
+			gesamtrate = gesamtrate + monatsrate;
+			System.out.println("Monat " + monat + " Saldo: " + saldo
+					+ " Gesamtratenzahlung: " + gesamtrate);
+			monat = monat + 1;
+		} while (saldo > monatsrate);
+
+		monatsrate = saldo + (saldo * zinssatz);
+		gesamtrate = gesamtrate + monatsrate;
+		endsaldo = saldo + (saldo * zinssatz) - monatsrate;
+		monat = monat + 1;
+		System.out.println("Monat " + monat + " Saldo: " + endsaldo
+				+ " Gesamtratenzahlung: " + gesamtrate);
+
+		scan.close();
+
+	}
 }
