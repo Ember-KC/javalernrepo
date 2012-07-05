1d684e4c5b94d4b0e9e7d176daa9e1ec85ae7345
Kathrin <Kathrin@KSiegmann-NB.fritz.box>
2012-07-05 21:50:02 +0200

    Nur positive Werte als Eingabe erlaubt.

--- KathrinsLernprojekt/src/kathrin/Ratenzahlung.java
+++ KathrinsLernprojekt/src/kathrin/Ratenzahlung.java
@@ -26,31 +26,35 @@ public class Ratenzahlung {
 		System.out.println("Bitte geben Sie den Zinssatz ein!");
 		zinssatz = scan.nextDouble();
 
-		if (monatsrate > saldo * zinssatz) {
+		if (monatsrate > 0 && saldo > 0 && zinssatz > 0) {
 
-			do {
-				saldo = saldo + (saldo * zinssatz) - monatsrate;
+			if (monatsrate > saldo * zinssatz) {
+
+				do {
+					saldo = saldo + (saldo * zinssatz) - monatsrate;
+					gesamtrate = gesamtrate + monatsrate;
+					System.out.println("Monat " + monat + " Saldo: " + saldo
+							+ " Gesamtratenzahlung: " + gesamtrate);
+					monat = monat + 1;
+				} while (saldo > monatsrate);
+
+				monatsrate = saldo + (saldo * zinssatz);
 				gesamtrate = gesamtrate + monatsrate;
-				System.out.println("Monat " + monat + " Saldo: " + saldo
-						+ " Gesamtratenzahlung: " + gesamtrate);
+				endsaldo = saldo + (saldo * zinssatz) - monatsrate;
 				monat = monat + 1;
-			} while (saldo > monatsrate);
+				System.out.println("Monat " + monat + " Saldo: " + endsaldo
+						+ " Gesamtratenzahlung: " + gesamtrate);
+			}
 
-			monatsrate = saldo + (saldo * zinssatz);
-			gesamtrate = gesamtrate + monatsrate;
-			endsaldo = saldo + (saldo * zinssatz) - monatsrate;
-			monat = monat + 1;
-			System.out.println("Monat " + monat + " Saldo: " + endsaldo
-					+ " Gesamtratenzahlung: " + gesamtrate);
+			else {
+				System.out
+						.println("Leider können wir keinen Kredit geben. "
+								+ "Die Monatsrate ist zu niedrig. Die Monatsrate muss größer "
+								+ saldo * zinssatz + " sein");
+			}
+		} else {
+			System.out.println("Bitte geben Sie nur positive Werte ein!");
 		}
-
-		else {
-			System.out
-					.println("Leider können wir keinen Kredit geben. "
-							+ "Die Monatsrate ist zu niedrig. Die Monatsrate muss größer "
-							+ saldo * zinssatz + " sein");
-		}
-
 		scan.close();
 
 	}
