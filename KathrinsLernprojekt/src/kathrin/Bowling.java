f97a788c2610d29edc539e678aa56b878c611a2c
Kathrin <Kathrin@KSiegmann-NB.fritz.box>
2012-07-08 21:07:33 +0200

    Bowling mit arrays, aber mit halbfertige Prüfung zu Spares und Strikes

--- KathrinsLernprojekt/src/kathrin/Bowling.java
+++ KathrinsLernprojekt/src/kathrin/Bowling.java
@@ -10,27 +10,52 @@ public class Bowling {
 	public static void main(String[] args) {
 		int runde = 1;
 		int gesamtpunkte = 0;
+		int ergebnisWurf1 = 0;
+		int ergebnisWurf2 = 0;
 		int rundenpunkte = 0;
+		int i = 0;
+
+		int wurf1[] = new int[10];
+		int wurf2[] = new int[10];
+		int punkteRunde[] = new int[10];
+		int strikeSpare[] = new int[10];
 
 		while (runde <= 10) {
 			System.out.println("Runde " + runde);
 			// erster Wurf
 			Random rand = new Random();
-			rundenpunkte = rand.nextInt(10) + 1;
+			ergebnisWurf1 = rand.nextInt(10) + 1;
+			wurf1[i] = ergebnisWurf1;
+			strikeSpare[i] = 1;
 
 			// zweiter Wurf
-			if (rundenpunkte < 10) {
-				rundenpunkte = rundenpunkte + rand.nextInt(10 - rundenpunkte)
-						+ 1;
+			if (ergebnisWurf1 < 10) {
+				ergebnisWurf2 = rand.nextInt(10 - ergebnisWurf1) + 1;
+				wurf2[i] = ergebnisWurf2;
+
+				// Verhalten des Programms bei einem Spare, d. h. 10 Pins bei
+				// beiden
+				// Würfen
+				if (ergebnisWurf1 + ergebnisWurf2 == 10) {
+					strikeSpare[i] = 2;
+				}
+			}
+			// Verhalten des Programms bei einem Strike, d. h. 10 Pins bei einem
+			// Wurf
+			else {
+
 			}
 			// Rundenergebnis ausgeben und Rundenpunkte zu Gesamtpunkten
 			// addieren
+			rundenpunkte = ergebnisWurf1 + ergebnisWurf2;
+			punkteRunde[i] = rundenpunkte;
 			System.out.println("Ergebnis der Runde " + runde + ": "
 					+ rundenpunkte);
 			gesamtpunkte = gesamtpunkte + rundenpunkte;
 
 			// Runde hochzählen
 			runde = runde + 1;
+			i++;
 		}
 		System.out.println("Gesamtpunktzahl: " + gesamtpunkte);
 
