470d2c01d100f2ca61ec9c67f42056217e9fed79
Kathrin <Kathrin@KSiegmann-NB.fritz.box>
2012-10-02 19:30:27 +0200

    AnredeVergeben mit if-Statement

--- KathrinsLernprojekt/src/kathrin/AnredeVergeben.java
+++ KathrinsLernprojekt/src/kathrin/AnredeVergeben.java
@@ -8,33 +8,13 @@ public class AnredeVergeben {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String geschlecht;
 
 		System.out.println("Bitte geben Sie Ihren Namen ein.");
 		Scanner scan = new Scanner(System.in);
 		String name = scan.nextLine();
 
-		switch (name) {
-		case "Amy":
-			geschlecht = "female";
-			break;
-		case "Buffy":
-			geschlecht = "female";
-			break;
-		case "Cathy":
-			geschlecht = "female";
-			break;
-		case "Elroy":
-			geschlecht = "male";
-			break;
-		case "Fred":
-			geschlecht = "male";
-			break;
-		default:
-			geschlecht = "";
-		}
-
-		if (geschlecht.equals("female")) {
+		if (name.startsWith("Amy") || name.startsWith("Buffy")
+				|| name.startsWith("Cathy")) {
 			System.out.println("Mrs. " + name);
 		} else {
 			System.out.println("Mr. " + name);
